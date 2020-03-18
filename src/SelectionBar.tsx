dad1427080f8484db6911d2a8c810dcf0d91e17f
Geo Perez <geovanni.perez@gmail.com>
2020-03-18 13:33:23 -0600

    Test with another import

--- src/SelectionBar.tsx
+++ src/SelectionBar.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Stack from 'office-ui-fabric-react/lib/components/Stack/Stack';
-import { Selection } from 'office-ui-fabric-react/lib/Utilities/selection/Selection';
+import { Selection } from '@uifabric/utilities';
 import { Label } from 'office-ui-fabric-react/lib/components/Label/Label';
 import { ICommandBarItemProps } from 'office-ui-fabric-react/lib/components/CommandBar/CommandBar.types';
 import { CommandBar } from 'office-ui-fabric-react/lib/components/CommandBar/CommandBar';
