050169a32ffa20a358052fd353f62d856ab1191b
Erick Garcia <erick.garcia@unosquare.com>
2020-06-10 11:13:30 -0500

    LinkCell

--- test/LinkCell.spec.tsx
+++ test/LinkCell.spec.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import { initializeIcons } from '@fluentui/react';
-import { CheckboxCell } from '../src/cells/index';
+import { LinkCell } from '../src/cells/index';
 import { render } from '@testing-library/react';
 
 initializeIcons();
 
-describe('CheckboxCell', () => {
+describe('LinkCell', () => {
     it('should render initial state w/o problem', async () => {
-        const { container } = render(<CheckboxCell checked={true} />);
+        const onClick = ()=>{};
+        const { container } = render(<LinkCell value='Title' onClick={onClick} />);
         expect(container).toBeDefined();
     });
 });
