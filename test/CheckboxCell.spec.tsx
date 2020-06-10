a2e9dd0c193881d80997fea1a08660a4a61bca14
Erick Garcia <erick.garcia@unosquare.com>
2020-06-10 14:48:45 -0500

    ClearDateButton tested

--- test/CheckboxCell.spec.tsx
+++ test/CheckboxCell.spec.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { initializeIcons } from '@fluentui/react';
 import { CheckboxCell } from '../src/cells/index';
-import { render } from '@testing-library/react';
+import { render, getByRole } from '@testing-library/react';
 
 initializeIcons();
 
 describe('CheckboxCell', () => {
     it('should render initial state w/o problem', async () => {
         const { container } = render(<CheckboxCell checked={true} />);
-        expect(container.firstChild.firstChild.firstChild).toBeTruthy();
+        expect(getByRole(container, 'checkbox')).toBeDefined();
     });
 });
