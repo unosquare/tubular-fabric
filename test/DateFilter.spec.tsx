1f95fd8d4102e362652b143f04cd8ae8c4edd253
Erick Garcia <erick.garcia@unosquare.com>
2020-06-11 14:59:27 -0500

    DateFilter improved

--- test/DateFilter.spec.tsx
+++ test/DateFilter.spec.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { initializeIcons } from '@fluentui/react';
 import { DateFilter } from '../src/filterControls/DateFilter';
-import { render, getByRole, getAllByRole } from '@testing-library/react';
+import { render, getAllByRole } from '@testing-library/react';
 import { mockColumn } from '../src/mocks';
 import { CompareOperators } from 'tubular-common';
 
@@ -9,7 +9,17 @@ initializeIcons();
 
 describe('DateFilter', () => {
     it('should render DateFilter initial state w/o problem', async () => {
-        const { container } = render(<DateFilter column={{...mockColumn, filterOperator: CompareOperators.Between}} onApply={() => {}}  />);
+        const { container } = render(<DateFilter column={{...mockColumn }} onApply={() => {}}  />);
+        expect(getAllByRole(container, 'button')).toBeDefined();
+    });
+
+    it('should render DateFilter initial state w/o problem with filter operator: Between', async () => {
+        const { container } = render(<DateFilter column={{...mockColumn, filterOperator: CompareOperators.Between }} onApply={() => {}}  />);
+        expect(getAllByRole(container, 'button').length).toBe(2);
+    });
+
+    it('should render DateFilter initial state w/o problem with startDate', async () => {
+        const { container } = render(<DateFilter column={{...mockColumn, filterOperator: CompareOperators.Between, filterText: '01/01/2020' }} onApply={() => {}}  />);
         expect(getAllByRole(container, 'button').length).toBe(2);
     });
 });
