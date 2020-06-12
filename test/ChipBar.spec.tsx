68d82fbe22ea3ae702529c31f27c3c3a95cb8329
Erick Garcia <erick.garcia@unosquare.com>
2020-06-12 12:24:38 -0500

    ChipFilter tested

--- test/ChipBar.spec.tsx
+++ test/ChipBar.spec.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import { initializeIcons } from '@fluentui/react';
 import { ChipBar } from '../src/index';
 import { render, getByRole, fireEvent } from '@testing-library/react';
 import { mockColumn } from '../src/mocks';
+import { initializeIcons } from '@fluentui/react';
 
 initializeIcons();
 
