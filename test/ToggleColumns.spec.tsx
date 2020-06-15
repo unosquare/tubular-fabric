0f142764fe4b2fd9d3cfdb28f1c6f701971142d7
Erick Garcia <erick.garcia@unosquare.com>
2020-06-15 15:05:54 -0500

    FeaturesPanel tested

--- test/ToggleColumns.spec.tsx
+++ test/ToggleColumns.spec.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import { initializeIcons } from '@fluentui/react';
 import { ToggleColumns } from '../src/ToggleColumns';
-import { render, getAllByRole, fireEvent, getByRole } from '@testing-library/react';
+import { render, getAllByRole, getByRole, fireEvent } from '@testing-library/react';
 import { mockColumn } from '../src/mocks';
-import { CompareOperators, ColumnDataType, ColumnModel } from 'tubular-common';
+import { ColumnModel } from 'tubular-common';
 
 initializeIcons();
 
-describe('FiltersContainer', () => {
-    it('should render FiltersContainer initial state w/o problem', async () => {
+describe('ToggleColumns', () => {
+    it('should render ToggleColumns initial state w/o problem', async () => {
         const { container } = render(<ToggleColumns columns={[mockColumn]} setColumns={(colums: ColumnModel[]) => {}} />);
 
         expect(getAllByRole(container, 'checkbox').length > 0).toBeTruthy();
