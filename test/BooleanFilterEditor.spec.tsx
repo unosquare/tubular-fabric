972760bca81312a08ad0eeab530dcaee3f457bd8
Erick Garcia <erick.garcia@unosquare.com>
2020-06-18 13:54:08 -0500

    BooleanFilterEditor tested

--- test/BooleanFilterEditor.spec.tsx
+++ test/BooleanFilterEditor.spec.tsx
@@ -1,26 +1,36 @@
 import * as React from 'react';
 import { initializeIcons } from '@fluentui/react';
 import { BooleanFilterEditor } from '../src/filterControls/BooleanFilterEditor';
-import { render, getByRole, fireEvent, getAllByRole } from '@testing-library/react';
+import { render, fireEvent, getAllByRole } from '@testing-library/react';
 import { mockColumn } from './mock';
 
 initializeIcons();
 
 describe('BooleanFilterEditor', () => {
     it('should render BooleanFilterEditor initial state w/o problem', async () => {
-        const { container } = render(<BooleanFilterEditor column={{...mockColumn, filterText: 'all'}} onApply={() => {}} />);
+        const { container } = render(<BooleanFilterEditor column={{...mockColumn, filterText: null}} onApply={() => {}} />);
         
         const firstRadioBuutton = getAllByRole(container, 'radio')[0];
-
 
         expect(firstRadioBuutton).toBeDefined();
     });
 
     it('should render BooleanFilterEditor initial state w/o problem and click the firstRadioButton', async () => {
-        const { container } = render(<BooleanFilterEditor column={{...mockColumn, filterText: null}} onApply={() => {}} />);
+        const { container } = render(<BooleanFilterEditor column={{...mockColumn}} onApply={() => {}} />);
         
         const firstRadioBuutton = getAllByRole(container, 'radio')[0];
 
         expect(fireEvent.click(firstRadioBuutton)).toBeTruthy();
+    });
+
+    it('Key value should be all', async () => {
+        const { container, debug } = render(<BooleanFilterEditor column={{...mockColumn, name: 'all'}} onApply={() => {}} />);
+        
+        const allRadios = getAllByRole(container, 'radio');
+        const lastRadioBuutton = allRadios[allRadios.length - 1];
+
+        debug(lastRadioBuutton);
+
+        expect(fireEvent.click(lastRadioBuutton)).toBeTruthy();
     });
 });
