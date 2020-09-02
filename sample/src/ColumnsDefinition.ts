a7b5df2e7a887b0a153e0d179e5e313e0c45abd2
Alejandro Ocampo <kadosh.ivan@gmail.com>
2020-09-02 14:27:55 -0500

    Fixed viewport issue when command bar is not present (#360)

--- sample/src/ColumnsDefinition.ts
+++ sample/src/ColumnsDefinition.ts
@@ -6,7 +6,7 @@ export const columns: ITbColumn[] = [
         key: 'OrderID',
         name: 'Order ID',
         fieldName: 'OrderID',
-        minWidth: 50,
+        minWidth: 80,
         maxWidth: 130,
         isResizable: true,
         tb: {
@@ -47,8 +47,7 @@ export const columns: ITbColumn[] = [
         key: 'Amount',
         name: 'Amount',
         fieldName: 'Amount',
-        minWidth: 120,
-        maxWidth: 200,
+        minWidth: 220,
         tb: {
             dataType: ColumnDataType.Numeric,
             sortable: true,
@@ -58,8 +57,7 @@ export const columns: ITbColumn[] = [
         key: 'IsShipped',
         name: 'Is Shipped?',
         fieldName: 'IsShipped',
-        minWidth: 60,
-        maxWidth: 80,
+        minWidth: 220,
         tb: {
             dataType: ColumnDataType.Boolean,
             sortable: true,
@@ -70,8 +68,7 @@ export const columns: ITbColumn[] = [
         key: 'ShippedDate',
         name: 'Shipped Date',
         fieldName: 'ShippedDate',
-        minWidth: 60,
-        maxWidth: 80,
+        minWidth: 220,
         tb: {
             dataType: ColumnDataType.Date,
             sortable: true,
