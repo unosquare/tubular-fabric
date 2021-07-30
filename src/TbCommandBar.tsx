e226e0bd0a69dfc70f8fd691204df1b8b72d54af
Noel Camacho <ncamacho0@ucol.mx>
2021-07-30 12:05:07 -0500

    Chore/UpdateTubularVersion Working well  (#601)
    * Chore/UpdateTubularVersion
    
    * fix tests
    
    * coverageThreshold
    
    * PropTypes
    
    * lint

--- src/TbCommandBar.tsx
+++ src/TbCommandBar.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
+
 import { CommandBar, ICommandBarItemProps } from '@fluentui/react/lib/CommandBar';
+
 import { ChipBar } from './ChipBar';
 import { registerTbIcons, getPagingMessage } from './utils';
 import { SearchBox, ISearchBoxStyles } from '@fluentui/react/lib/SearchBox';
 import { IFabricTbState, ITbFabricApi } from './interfaces';
 import { FeaturesPanel } from './FeaturesPanel';
+import * as PropTypes from 'prop-types';
 
 registerTbIcons();
 
@@ -33,10 +36,10 @@ export const TbCommandBar: React.FunctionComponent<TbCommandBarProps> = ({
     const closePanel = () => setShowPanel(false);
     const openPanel = () => setShowPanel(true);
 
-    const label = React.useMemo(() => getPagingMessage(tbState.totalRecordCount, tbState.filteredRecordCount), [
-        tbState.totalRecordCount,
-        tbState.filteredRecordCount,
-    ]);
+    const label = React.useMemo(
+        () => getPagingMessage(tbState.totalRecordCount, tbState.filteredRecordCount),
+        [tbState.totalRecordCount, tbState.filteredRecordCount],
+    );
 
     const _farItems: ICommandBarItemProps[] = [];
     const onClear = () => tbApi.search();
@@ -96,4 +99,9 @@ export const TbCommandBar: React.FunctionComponent<TbCommandBarProps> = ({
             )}
         </>
     );
+};
+
+TbCommandBar.propTypes = {
+    tbState: PropTypes.any,
+    tbApi: PropTypes.any,
 };
